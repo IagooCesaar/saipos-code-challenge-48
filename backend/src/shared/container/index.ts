e2122ca9d3ed3e202d5ec2849f9a3277f802e533
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-21 11:33:56 -0300

    container TaskHistoryRepository

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { container } from "tsyringe";
 import "./providers";
 
+import { TaskHistoryRepository } from "@modules/tasks/infra/repositories/TaskHistoryRepository";
 import { TasksRepository } from "@modules/tasks/infra/repositories/TasksRepository";
+import { ITaskHistoryRepository } from "@modules/tasks/repositories/ITaskHistoryRepository";
 import { ITasksRepository } from "@modules/tasks/repositories/ITasksRepository";
 import { UsersRepository } from "@modules/users/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
@@ -14,4 +16,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ITasksRepository>(
   "TasksRepository",
   TasksRepository
+);
+
+container.registerSingleton<ITaskHistoryRepository>(
+  "TaskHistoryRepository",
+  TaskHistoryRepository
 );
